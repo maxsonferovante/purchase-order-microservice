5d206907eb9f26a2536d01cf1ef7a6d98a0d41c2
Maxson Almeida <maxsonferovante@gmail.com>
2024-08-31 16:02:36 -0300

    feat: Refactor OrderRepository create_order method
    This commit refactors the `create_order` method in the `OrderRepository` class. Instead of accepting a dictionary as a parameter, it now accepts an `Order` object. The method inserts the order into the MongoDB collection and updates the `order_id` field of the `Order` object with the generated ID. This change improves the clarity and type safety of the codebase.

--- purchase_order_microservice/infra/database/mongo/repositories/order_repository.py
+++ purchase_order_microservice/infra/database/mongo/repositories/order_repository.py
@@ -2,23 +2,23 @@ from bson.objectid import ObjectId
 from typing import Dict, List
 from purchase_order_microservice.infra.database.mongo.settings.connection import MongoConnectionHandler
 from purchase_order_microservice.data.interfaces.order_repository_interface import OrderRepositoryInterface
-
+from purchase_order_microservice.domain.models.order import Order
 
 class OrderRepository(OrderRepositoryInterface):
     
     collection_name = "OrderCollection"
     
     @classmethod
-    def create_order(cls, order_document: Dict) -> Dict:
+    def create_order(cls, order: Order) -> Order:
         with MongoConnectionHandler() as mongo_connection_handler:
             order_collection = mongo_connection_handler.get_db_connection()[cls.collection_name]
-            result = order_collection.insert_one(order_document)
-            order_document["order_id"] = str(result.inserted_id)
-            return order_document
+            result = order_collection.insert_one(order.to_dict())
+            order.order_id = str(result.inserted_id)
+            return order
     
             
     @classmethod
-    def get_order_by_id(cls, order_id: str) -> Dict:
+    def get_order_by_id(cls, order_id: str) -> Order:
         with MongoConnectionHandler() as mongo_connection_handler:
             order_collection = mongo_connection_handler.get_db_connection()[cls.collection_name]
             result = order_collection.find_one(
